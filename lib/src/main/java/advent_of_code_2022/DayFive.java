da5a4cc2912279c621a4ca244dd4f4c54e7b0384
alec.tunbridge <alec.tunbridge@gmail.com>
2022-12-05 18:50:41 +0000

    Solves part 2

--- lib/src/main/java/advent_of_code_2022/DayFive.java
+++ lib/src/main/java/advent_of_code_2022/DayFive.java
@@ -6,38 +6,37 @@ import java.util.regex.Pattern;
 
 public class DayFive {
 
+    private final List<Deque<String>> stacks;
     private List<String> input;
 
     public DayFive(List<String> input) {
         this.input = input;
+        stacks = new ArrayList<>();
     }
 
     public String solvePart1() {
-
-        List<Deque<String>> stacks = new ArrayList<>();
-
         boolean firstLine = true;
         for (String line : input) {
-            if(firstLine){
+            if (firstLine) {
                 int noOfStacks = (line.length() + 1) / 4;
                 for (int i = 0; i < noOfStacks; i++) {
                     stacks.add(new ArrayDeque<>());
                 }
                 firstLine = false;
             }
-            if(line.contains("[")){
+            if (line.contains("[")) {
                 Pattern cratePattern = Pattern.compile(".(.)..?");
                 Matcher matcher = cratePattern.matcher(line);
                 int stackIndex = 0;
-                while(matcher.find()){
+                while (matcher.find()) {
                     String crateLetter = matcher.group(1);
-                    if (!crateLetter.isBlank()){
+                    if (!crateLetter.isBlank()) {
                         stacks.get(stackIndex).addLast(crateLetter);
                     }
                     stackIndex++;
                 }
 
-            } else if(line.matches(".*[a-z].*")) {
+            } else if (line.matches(".*[a-z].*")) {
 
                 String[] parts = line.split(" ");
 
@@ -59,15 +58,59 @@ public class DayFive {
                 stringBuilder.append(stack.pop());
             }
         }
-
-        System.out.println(stringBuilder);
-
         return stringBuilder.toString();
     }
 
     public String solvePart2() {
+        boolean firstLine = true;
+        for (String line : input) {
+            if (firstLine) {
+                int noOfStacks = (line.length() + 1) / 4;
+                for (int i = 0; i < noOfStacks; i++) {
+                    stacks.add(new ArrayDeque<>());
+                }
+                firstLine = false;
+            }
+            if (line.contains("[")) {
+                Pattern cratePattern = Pattern.compile(".(.)..?");
+                Matcher matcher = cratePattern.matcher(line);
+                int stackIndex = 0;
+                while (matcher.find()) {
+                    String crateLetter = matcher.group(1);
+                    if (!crateLetter.isBlank()) {
+                        stacks.get(stackIndex).addLast(crateLetter);
+                    }
+                    stackIndex++;
+                }
 
-        return "";
+            } else if (line.matches(".*[a-z].*")) {
+
+                String[] parts = line.split(" ");
+
+                int quantity = Integer.parseInt(parts[1]);
+                int from = Integer.parseInt(parts[3]);
+                int to = Integer.parseInt(parts[5]);
+
+                Deque<String> intermediateStack = new ArrayDeque<>();
+                for (int i = 0; i < quantity; i++) {
+                    String pop = stacks.get(from - 1).pop();
+                    intermediateStack.push(pop);
+                }
+                for(String crate:intermediateStack) {
+                    stacks.get(to - 1).push(crate);
+                }
+            } else {
+                //do nowt
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Deque<String> stack : stacks) {
+            if (!stack.isEmpty()) {
+                stringBuilder.append(stack.pop());
+            }
+        }
+        return stringBuilder.toString();
     }
-
 }
+
--- lib/src/test/java/advent_of_code_2022/DayFiveTest.java
+++ lib/src/test/java/advent_of_code_2022/DayFiveTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DayFiveTest {
 
     @Test
-    void solvePart1And2Test() {
+    void solvePart1Example() {
         DayFive dayFive = new DayFive(List.of(
                 "    [D]    ",
                 "[N] [C]    ",
@@ -22,13 +22,31 @@ public class DayFiveTest {
                 "move 1 from 1 to 2"
         ));
         assertThat(dayFive.solvePart1()).isEqualTo("CMZ");
-        //assertThat(dayFour.solvePart2()).isEqualTo(859);
+    }
+    @Test
+    void solvePart2Example() {
+        DayFive dayFive = new DayFive(List.of(
+                "    [D]    ",
+                "[N] [C]    ",
+                "[Z] [M] [P]",
+                " 1   2   3",
+                "",
+                "move 1 from 2 to 1",
+                "move 3 from 1 to 3",
+                "move 2 from 2 to 1",
+                "move 1 from 1 to 2"
+        ));
+        assertThat(dayFive.solvePart2()).isEqualTo("MCD");
     }
 
     @Test
     void solvePart1() {
         DayFive dayFive = new DayFive(Utils.readInputLinesFromFile("day_five.txt"));
         assertThat(dayFive.solvePart1()).isEqualTo("FCVRLMVQP");
-        //assertThat(dayFour.solvePart2()).isEqualTo(859);
+    }
+    @Test
+    void solvePart2() {
+        DayFive dayFive = new DayFive(Utils.readInputLinesFromFile("day_five.txt"));
+        assertThat(dayFive.solvePart2()).isEqualTo("RWLWGJGFD");
     }
 }
