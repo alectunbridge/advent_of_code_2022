07347ab1d2b4678e74d7164e652b2b971d1c433d
Shaun Storey <shaun.storey@autotrader.co.uk>
2022-12-05 10:03:58 +0000

    Day five part 1

---
+++ lib/src/test/java/advent_of_code_2022/DayFiveTest.java
@@ -0,0 +1,24 @@
+package advent_of_code_2022;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DayFiveTest {
+
+    @Test
+    void solvePart1And2Test(){
+        DayFive dayFive = new DayFive(Utils.readInputLinesFromFile("testinput.txt"));
+        assertThat(dayFive.solvePart1()).isEqualTo("CMZ");
+        //assertThat(dayFour.solvePart2()).isEqualTo(859);
+    }
+
+    @Test
+    void solvePart1(){
+        DayFive dayFive = new DayFive(Utils.readInputLinesFromFile("day_five.txt"));
+        assertThat(dayFive.solvePart1()).isEqualTo("CMZ");
+        //assertThat(dayFour.solvePart2()).isEqualTo(859);
+    }
+}
