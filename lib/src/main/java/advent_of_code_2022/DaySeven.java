77c970d847f4479d23a325df43404529bb94a4ff
alec.tunbridge <alec.tunbridge@gmail.com>
2022-12-07 20:50:26 +0000

    never gonna work
    directory names aren't uniq

--- lib/src/main/java/advent_of_code_2022/DaySeven.java
+++ lib/src/main/java/advent_of_code_2022/DaySeven.java
@@ -1,38 +1,78 @@
 package advent_of_code_2022;
 
-import java.util.List;
+import com.google.common.base.MoreObjects;
+import com.google.common.cache.AbstractCache;
+
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Directory {
 
     private String name;
+    private int size;
+    private Set<String> subdirectories;
 
     public Directory(String name) {
-
+        this.subdirectories = new HashSet<>();
         this.name = name;
+    }
+
+    public void addToSize(int size) {
+        this.size += size;
+    }
+
+    public int getSize() {
+        int subDirectorySizes = subdirectories.stream().mapToInt(sub -> DaySeven.directories.get(sub).getSize()).sum();
+        return size + subDirectorySizes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addSubDirectory(String subdirectory) {
+        subdirectories.add(subdirectory);
     }
 }
 
 public class DaySeven {
-    private static final String CD_PATTERN = "\\$ cd (<?name>\\w*)";
     private List<String> input;
+    static Map<String, Directory> directories;
 
-    
+
     public DaySeven(List<String> input) {
-
+        directories = new HashMap<>();
         this.input = input;
     }
 
     public int solvePart1() {
-        for (int i = 0; i < input.size(); i++) {
-            String line = input.get(i);
-            Matcher cdMatcher = Pattern.compile(CD_PATTERN).matcher(line);
-            if(cdMatcher.matches()){
-                Directory directory = new Directory(cdMatcher.group("name"));   
+        Directory currentDirectory = null;
+        for (int i = 0; i < input.size(); ) {
+            if (input.get(i).matches("\\$ cd ..")){
+                System.out.println("skipping: " + input.get(i));
             }
-            
+            if (input.get(i).matches("\\$ cd [^.]+")) {
+                currentDirectory = new Directory(input.get(i).split(" ")[2]);
+                i++;
+                while (i<input.size()) {
+                    if (input.get(i).matches("\\$ ls")){
+                        //skip
+                    } else if (input.get(i).matches("^\\d.*")) {
+                        currentDirectory.addToSize(Integer.parseInt(input.get(i).split(" ")[0]));
+                    } else if (input.get(i).matches("dir \\w+")){
+                        currentDirectory.addSubDirectory(input.get(i).split(" ")[1]);
+                    } else if (input.get(i).startsWith("$")){
+                        directories.put(currentDirectory.getName(), currentDirectory);
+                        i--;
+                        break;
+                    }
+                    i++;
+                }
+            }
+            i++;
         }
-        return 0;
+        directories.put(currentDirectory.getName(), currentDirectory);
+        return directories.values().stream().mapToInt(Directory::getSize).filter(i->i<=100000).sum();
     }
 }
--- lib/src/test/java/advent_of_code_2022/DaySevenTest.java
+++ lib/src/test/java/advent_of_code_2022/DaySevenTest.java
@@ -11,14 +11,75 @@ public class DaySevenTest {
     void parseSingleDirectory() {
         DaySeven daySeven = new DaySeven(
                 List.of(
-                        "$ cd d",
+                        "$ cd e",
                         "$ ls",
-                        "4060174 j",
-                        "8033020 d.log",
-                        "5626152 d.ext",
-                        "7214296 k"
+                        "584 i"
                 )
         );
-        assertThat(daySeven.solvePart1()).isEqualTo(4060174+8033020+5626152+7214296);
+        assertThat(daySeven.solvePart1()).isEqualTo(584);
+    }
+
+    @Test
+    void testMultipleSimpleDirectories() {
+        DaySeven daySeven = new DaySeven(List.of(
+                "$ cd e",
+                "$ ls",
+                "584 i",
+                "$ cd f",
+                "$ ls",
+                "584 i"
+        ));
+        assertThat(daySeven.solvePart1()).isEqualTo(584+584);
+    }
+    
+    @Test
+    void testNestedDirectories() {
+        DaySeven daySeven = new DaySeven(List.of(
+                "$ cd e",
+                "$ ls",
+                "584 i",
+                "dir f",
+                "$ cd f",
+                "$ ls",
+                "1 i"
+        ));
+        assertThat(daySeven.solvePart1()).isEqualTo(584+1+1);
+    }
+    
+    @Test
+    void partOneExample(){
+        DaySeven daySeven = new DaySeven(List.of(
+                "$ cd /",
+                "$ ls",
+                "dir a",
+                "14848514 b.txt",
+                "8504156 c.dat",
+                "dir d",
+                "$ cd a",
+                "$ ls",
+                "dir e",
+                "29116 f",
+                "2557 g",
+                "62596 h.lst",
+                "$ cd e",
+                "$ ls",
+                "584 i",
+                "$ cd ..",
+                "$ cd ..",
+                "$ cd d",
+                "$ ls",
+                "4060174 j",
+                "8033020 d.log",
+                "5626152 d.ext",
+                "7214296 k"              
+        ));
+        
+        assertThat(daySeven.solvePart1()).isEqualTo(95437);
+    }
+
+    @Test
+    void part1() {
+        DaySeven daySeven = new DaySeven(Utils.readInputLinesFromFile("day_seven.txt"));
+        assertThat(daySeven.solvePart1()).isEqualTo(0);
     }
 }
