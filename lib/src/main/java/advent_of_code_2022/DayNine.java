83eb6a1d40569b48f17832b2825d83c93f0bb835
alec.tunbridge <alec.tunbridge@gmail.com>
2022-12-11 20:42:07 +0000

    WIP - transferred from netbook

--- lib/src/main/java/advent_of_code_2022/DayNine.java
+++ lib/src/main/java/advent_of_code_2022/DayNine.java
@@ -1,23 +1,98 @@
 package advent_of_code_2022;
 
+import java.util.*;
+class CoordDayNine {
+    int x, y;
 
-import java.util.List;
+    CoordDayNine(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
-public class DayNine {
-
+class DayNine {
     private List<String> input;
+    private Set<String> tailPositions;
+    private List<CoordDayNine> knots;
 
-    public DayNine(List<String> input) {
+    DayNine(List<String> input) {
         this.input = input;
+        this.tailPositions = new HashSet<>();
+        this.knots = new ArrayList<>();
     }
 
     public int solvePart1() {
-
-        return 0;
+        return solveForNumberOfKnots(2);
     }
 
     public int solvePart2() {
+        return solveForNumberOfKnots(10);
+    }
 
-        return 0;
+    public int solveForNumberOfKnots(int noOfKnots) {
+        for (int i = 0; i < noOfKnots; i++) {
+            knots.add(new CoordDayNine(0, 0));
+        }
+
+        tailPositions.add("0,0");
+
+        for (String line : input) {
+            String[] tokens = line.split(" ");
+            int xDelta = 0;
+            int yDelta = 0;
+
+            switch (tokens[0]) {
+                case "R":
+                    xDelta = 1;
+                    yDelta = 0;
+                    break;
+                case "L":
+                    xDelta = -1;
+                    yDelta = 0;
+                    break;
+                case "U":
+                    xDelta = 0;
+                    yDelta = 1;
+                    break;
+                case "D":
+                    xDelta = 0;
+                    yDelta = -1;
+                    break;
+            }
+
+
+            for (int step = 0; step < Integer.parseInt(tokens[1]); step++) {
+
+                knots.get(0).x += xDelta;
+                knots.get(0).y += yDelta;
+
+                for (int index = 0; index < knots.size() - 1; index++) {
+                    moveTail(knots.get(index), knots.get(index + 1));
+                }
+
+                tailPositions.add("" + knots.get(knots.size() - 1).x + "," + knots.get(knots.size() - 1).y);
+
+            }
+
+        }
+
+        return tailPositions.size();
+    }
+
+    private void moveTail(CoordDayNine head, CoordDayNine tail) {
+        int tailXDelta = (-tail.x + head.x) / 2;
+        int tailYDelta = (-tail.y + head.y) / 2;
+        if (Math.abs(tailXDelta) == 1) {
+            tail.y = head.y;
+        }
+        if (Math.abs(tailYDelta) == 1) {
+            tail.x = head.x;
+        }
+        tail.x += tailXDelta;
+        tail.y += tailYDelta;
+    }
+
+    public Set<String> getTailPositions() {
+        return tailPositions;
     }
 }
--- lib/src/test/java/advent_of_code_2022/DayNineTest.java
+++ lib/src/test/java/advent_of_code_2022/DayNineTest.java
@@ -2,6 +2,7 @@ package advent_of_code_2022;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,5 +29,50 @@ class DayNineTest {
     void solvePart1() {
         DayNine dayNine = new DayNine(Utils.readInputLinesFromFile("day_nine.txt"));
         assertThat(dayNine.solvePart1()).isEqualTo(1698);
+    }
+
+    @Test
+    void part2Example() {
+        DayNine part2Example = new DayNine(Arrays.asList(
+                "R 5",
+                "U 8",
+                "L 8",
+                "D 3",
+                "R 17",
+                "D 10",
+                "L 25",
+                "U 20"
+        ));
+        int result = part2Example.solvePart2();
+        System.out.println(part2Example.getTailPositions());
+        assertThat(result).isEqualTo(36);
+    }
+        
+
+    public static void main(String[] args) {
+        DayNine dayNine = new DayNine(Arrays.asList(
+                "R 4",
+                "U 4",
+                "L 3",
+                "D 1",
+                "R 4",
+                "D 1",
+                "L 5",
+                "R 2"
+        ));
+        int result = dayNine.solvePart1();
+
+        System.out.println(dayNine.getTailPositions());
+        assert result == 13 : "part one example expected 13 but was " + result;
+
+        DayNine part1 = new DayNine(Utils.readInputLinesFromFile("day_nine.txt"));
+        result = part1.solvePart1();
+        assert result == 6522 : "part one expected ? but was " + result;
+
+
+        DayNine part2 = new DayNine(Utils.readInputLinesFromFile("day_nine.txt"));
+        result = part2.solvePart2();
+        assert result == 0 : "part two expected ? but was " + result;
+
     }
 }
